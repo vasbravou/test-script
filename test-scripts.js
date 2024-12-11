ff961d3461281390bf1ddc674153e282295e4b7d
vasbravou <45942592+vasbravou@users.noreply.github.com>
2024-12-11 16:17:14 +0200

    Update test-scripts.js

--- test-scripts.js
+++ test-scripts.js
@@ -1,10 +1,2 @@
-<script>
-window.addEventListener('productfruits_card_custom_btn_click', (e) =&gt; {
-	if (e.detail.id == 'my-button-1') {
-		console.log("my-button-1 clicked!");
-	}
-});
-</script>
-
-
-<script>console.log("test script run!");</script>
+console.log("Test script run!");
+document.body.innerHTML = "<h1>Script successfully loaded!</h1>";
