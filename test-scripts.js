cd649c19688ec306e64bb1bfe569c50387f6accb
vasbravou <45942592+vasbravou@users.noreply.github.com>
2024-12-11 16:11:12 +0200

    Add files via upload

---
+++ test-scripts.js
@@ -0,0 +1,10 @@
+<script>
+window.addEventListener('productfruits_card_custom_btn_click', (e) =&gt; {
+	if (e.detail.id == 'my-button-1') {
+		console.log("my-button-1 clicked!");
+	}
+});
+</script>
+
+
+<script>console.log("test script run!");</script>
